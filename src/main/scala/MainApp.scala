588b05e2758cad0ed57ef8ce17b0129c5b9ddc97
Gilbert Ziade <gilbert_z2001@hotmail.com>
2024-01-20 00:22:03 +0100

    Creating the Menu

--- src/main/scala/MainApp.scala
+++ src/main/scala/MainApp.scala
@@ -6,20 +6,135 @@ object MyApp extends ZIOAppDefault {
 
   def run = myAppLogic
 
-  val myAppLogic =
+  val myAppLogic = {
+
+    //Making the API Calls of spotify + writing them in a CSV
+    val apiCalls: APICalls = new APICalls()
+
+    apiCalls.extractToken()
+
+    apiCalls.writeTracksInCSV()
+    apiCalls.writeAlbumsInCSV()
+    apiCalls.writeArtistsInCSV()
+
     for {
-      _ <- printLine("Hello! What is your name?")
-      name <- readLine
-      _ <- printLine(s"Hello, ${name}, welcome to ZIO!")
+      _ <- printLine("Hello!\nYour App is ready to be used")
+      _ <- printLine(menuInput())
     } yield ()
+  }
 
+  def menuInput(): Unit = {
+    while(true) {
+      println("Choose the right number")
+      println("1. Tracks")
+      println("2. Albums")
+      println("3. Artists")
+      println("0. Exit")
+      val input: String = scala.io.StdIn.readLine()
+      if (input == "0") {
+        return false
+      }
+      else if (input == "1") {
+        println("You selected Tracks")
+        tracksInput()
+      }
+      else if (input == "2") {
+        println("You selected Albums")
+        albumsInput()
+      }
+      else if (input == "3") {
+        println("You selected Artists")
+        artistsInput()
+      }
+    }
+  }
 
-  //Making the API Calls of spotify + writing them in a CSV
-  val apiCalls: APICalls = new APICalls()
+  //MODIFY FROM HERE
+  def tracksInput(): Unit = {
+    println()
+    while (true) {
+      println("Choose the right number")
+      val input: String = scala.io.StdIn.readLine()
+      if (input == "0") {
+        return false
+      }
+      else if (input == "1") {
+        println("You selected Tracks1")
+      }
+      else if (input == "2") {
+        println("You selected Albums2")
+      }
+      else if (input == "3") {
+        println("You selected Artists3")
+      }
+      else if (input == "4") {
+        println("You selected Tracks1")
+      }
+      else if (input == "5") {
+        println("You selected Albums2")
+      }
+      else if (input == "6") {
+        println("You selected Artists3")
+      }
+    }
+  }
 
-  apiCalls.extractToken()
+  def albumsInput(): Unit = {
+    println()
+    while (true) {
+      println("Choose the right number")
+      val input: String = scala.io.StdIn.readLine()
+      if (input == "0") {
+        return false
+      }
+      else if (input == "1") {
+        println("You selected Tracks1")
+      }
+      else if (input == "2") {
+        println("You selected Albums2")
+      }
+      else if (input == "3") {
+        println("You selected Artists3")
+      }
+      else if (input == "4") {
+        println("You selected Tracks1")
+      }
+      else if (input == "5") {
+        println("You selected Albums2")
+      }
+      else if (input == "6") {
+        println("You selected Artists3")
+      }
+    }
+  }
 
-  apiCalls.writeTracksInCSV();
-  apiCalls.writeAlbumsInCSV()
-  apiCalls.writeArtistsInCSV()
+  def artistsInput(): Unit = {
+    println()
+    while (true) {
+      println("Choose the right number")
+      val input: String = scala.io.StdIn.readLine()
+      if (input == "0") {
+        return false
+      }
+      else if (input == "1") {
+        println("You selected Tracks1")
+      }
+      else if (input == "2") {
+        println("You selected Albums2")
+      }
+      else if (input == "3") {
+        println("You selected Artists3")
+      }
+      else if (input == "4") {
+        println("You selected Tracks1")
+      }
+      else if (input == "5") {
+        println("You selected Albums2")
+      }
+      else if (input == "6") {
+        println("You selected Artists3")
+      }
+    }
+  }
 }
+
