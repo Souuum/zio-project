64c2d8f7da6af815729789a8517469027adbfe5a
soum <hugo.soum-brugevin@efrei.net>
2024-01-19 17:40:36 +0100

    added getAll and getById

--- src/main/scala/Services/TrackService.scala
+++ src/main/scala/Services/TrackService.scala
@@ -1,3 +1,29 @@
 package services
+import entities._
+import zio._
+import zio.Console._
+import java.io.IOException
+import scala.collection.mutable.ListBuffer
+import repositories.implementation.TrackRepository
 
-//TODO
+object TrackeService {
+
+  def callGetAllTracks(): ZIO[Track, IOException, ListBuffer[Track]] = {
+    return TrackRepository.getAll()
+  }
+
+  def callGetTrackById(id: String): Option[Track] = {
+    return TrackRepository.getById(id)
+  }
+//trie les albums par popularité
+  def callSortTracksPopularityASC()
+      : ZIO[Track, IOException, ListBuffer[Track]] = {
+    return TrackRepository.getAllByAscPopularity()
+  }
+
+  def callSortTracksPopularityDESC()
+      : ZIO[Track, IOException, ListBuffer[Track]] = {
+    return TrackRepository.getAllByDescPopularity()
+  }
+
+}
