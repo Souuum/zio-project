5af70e035fab43edd6482d6e8ae9b0b5a3a5c3ca
soum <hugo.soum-brugevin@efrei.net>
2024-01-20 00:51:16 +0100

    [Missing]Infinite loop for menu

--- src/main/scala/MainApp.scala
+++ src/main/scala/MainApp.scala
@@ -1,32 +1,11 @@
 import batchs.APICalls
 import zio.*
 import zio.Console.*
+import repositories.implementation._
 
 object MyApp extends ZIOAppDefault {
 
   def run = myAppLogic
-
-  val myAppLogic =
-    for {
-      _ <- printLine("Hello! Welcome to the Spotify API")
-      _ <- printLine("Please wait while we are fetching the data")
-      _ <- printLine("Data fetched!")
-      _ <- ArtistRepository.getPopASC
-      _ <- ArtistRepository.getPopDESC
-      _ <- ArtistRepository.getByGenre("pop")
-      _ <- ArtistRepository.getByGenre("rock")
-      _ <- ArtistRepository.getByGenre("rap")
-      _ <- ArtistRepository.getByGenre("hip hop")
-      _ <- Console.printLine("------------------------------------------")
-      _ <- AlbumRepository.getPopASC
-      _ <- AlbumRepository.getPopDESC
-      _ <- AlbumRepository.getDateASC
-      _ <- AlbumRepository.getDateDESC
-      _ <- Console.printLine("------------------------------------------")
-      _ <- TrackRepository.getPopASC
-      _ <- TrackRepository.getPopDESC
-
-    } yield ()
 
   // Making the API Calls of spotify + writing them in a CSV
   val apiCalls: APICalls = new APICalls()
@@ -42,119 +21,63 @@ object MyApp extends ZIOAppDefault {
     apiCalls.writeArtistsInCSV()
 
     for {
-      _ <- printLine("Hello!\nYour App is ready to be used")
-      _ <- printLine(menuInput())
+      _ <- printLine("Hello! Welcome to the Spotify API")
+      _ <- printLine("Please wait while we are fetching the data")
+      _ <- printLine("Data fetched!")
+      _ <- printLine("------------------------------------------")
+      _ <- printLine("What would you like to do ?")
+      _ <- printLine(
+        "1. See the top 10 artists ordered by popularity ascending"
+      )
+      _ <- printLine(
+        "2. See the top 10 artists ordered by popularity descending"
+      )
+      _ <- printLine("3. See the artists from top 10 by musical genre")
+      _ <- printLine("4. See the top 10 albums ordered by popularity ascending")
+      _ <- printLine(
+        "5. See the top 10 albums ordered by popularity descending"
+      )
+      _ <- printLine("6. See the top 10 albums ordered by date ascending")
+      _ <- printLine("7. See the top 10 albums ordered by date descending")
+      _ <- printLine("8. See the top 10 tracks ordered by popularity ascending")
+      _ <- printLine(
+        "9. See the top 10 tracks ordered by popularity descending"
+      )
+      _ <- printLine("10. Quit")
+
+      answer <- readLine
+      answerInt = answer.toInt
+      _ <- answerInt match {
+        case 1 =>
+          ArtistRepository.getPopASC
+        case 2 =>
+          ArtistRepository.getPopDESC
+        case 3 => {
+          for {
+            _ <- printLine("Please enter a musical genre")
+            genre <- readLine
+            _ <- ArtistRepository.getByGenre(genre)
+          } yield ()
+        }
+        case 4 =>
+          AlbumRepository.getPopASC
+        case 5 =>
+          AlbumRepository.getPopDESC
+        case 6 =>
+          AlbumRepository.getDateASC
+        case 7 =>
+          AlbumRepository.getDateDESC
+        case 8 =>
+          TrackRepository.getPopASC
+        case 9 =>
+          TrackRepository.getPopDESC
+        case 10 =>
+          printLine("Goodbye!")
+        case _ =>
+          printLine("Please enter a valid number")
+      }
+
     } yield ()
   }
 
-  def menuInput(): Unit = {
-    while (true) {
-      println("Choose the right number")
-      println("1. Tracks")
-      println("2. Albums")
-      println("3. Artists")
-      println("0. Exit")
-      val input: String = scala.io.StdIn.readLine()
-      if (input == "0") {
-        return false
-      } else if (input == "1") {
-        println("You selected Tracks")
-        tracksInput()
-      } else if (input == "2") {
-        println("You selected Albums")
-        albumsInput()
-      } else if (input == "3") {
-        println("You selected Artists")
-        artistsInput()
-      }
-    }
-  }
-
-  // MODIFY FROM HERE
-  def tracksInput(): Unit = {
-    println()
-
-    println("1. ...")
-    println("2. ...")
-    println("3. ...")
-    println("0. Exit")
-
-    while (true) {
-      println("Choose the right number")
-      val input: String = scala.io.StdIn.readLine()
-      if (input == "0") {
-        return false
-      } else if (input == "1") {
-        println("You selected Tracks1")
-      } else if (input == "2") {
-        println("You selected Albums2")
-      } else if (input == "3") {
-        println("You selected Artists3")
-      } else if (input == "4") {
-        println("You selected Tracks1")
-      } else if (input == "5") {
-        println("You selected Albums2")
-      } else if (input == "6") {
-        println("You selected Artists3")
-      }
-    }
-  }
-
-  def albumsInput(): Unit = {
-    println()
-
-    println("1. ...")
-    println("2. ...")
-    println("3. ...")
-    println("0. Exit")
-
-    while (true) {
-      println("Choose the right number")
-      val input: String = scala.io.StdIn.readLine()
-      if (input == "0") {
-        return false
-      } else if (input == "1") {
-        println("You selected Tracks1")
-      } else if (input == "2") {
-        println("You selected Albums2")
-      } else if (input == "3") {
-        println("You selected Artists3")
-      } else if (input == "4") {
-        println("You selected Tracks1")
-      } else if (input == "5") {
-        println("You selected Albums2")
-      } else if (input == "6") {
-        println("You selected Artists3")
-      }
-    }
-  }
-
-  def artistsInput(): Unit = {
-    println()
-
-    println("1. ...")
-    println("2. ...")
-    println("3. ...")
-    println("0. Exit")
-
-    while (true) {
-      println("Choose the right number")
-      val input: String = scala.io.StdIn.readLine()
-      if (input == "0") {
-        return false
-      } else if (input == "1") {
-        println("You selected Tracks1")
-      } else if (input == "2") {
-        println("You selected Albums2")
-      } else if (input == "3") {
-        println("You selected Artists3")
-      } else if (input == "4") {
-        println("You selected Tracks1")
-      } else if (input == "5") {
-        println("You selected Albums2")
-      } else if (input == "6") {
-        println("You selected Artists3")
-      }
-    }
-  }
 }
